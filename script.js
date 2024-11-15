092baa22e43eaeac93fd22d4517fe7a7b22e9a42
dreadlord-sedai <dahamifabbio@gmail.com>
2024-11-15 19:50:38 +0530

    Code play round Logic

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Get Random computer choice
 function getComputerChoice() {
-    
+
     // 0 = rock, 1 = paper, 2 = scissors
     const randomnumber = Math.floor(Math.random() * 3);
     if (randomnumber === 0) {
@@ -38,7 +38,28 @@ let computerScore = 0;
 // Function to play a round
 function playRound(humanChoice, computerChoice) {
 
-    
+    // Logic to determine winner
+    if (humanChoice === computerChoice) {
+        return "It's a tie!" + humanScore + "and" + computerScore + "is the same.";
+    } else if (humanChoice === "rock && computerChoice === scissors") {
+        humanScore++;
+        return "You win! Rock beats scissors.";
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        computerScore++;
+        return "You lose! Paper beats rock.";
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        humanScore++;
+        return "You win! Paper beats scissors.";
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        computerScore++;
+        return "You lose! Rock beats paper.";
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++;
+        return "You win! Scissors beats paper.";
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        computerScore++;
+        return "You lose! Rock beats scissors.";
+    }
 }
 
 const computerChoice = getComputerChoice();
